4ccde3e9eb7dbab0242ea5aff102226ad331fa21
anshjyotsingh <anshjyot1504@gmail.com>
2023-03-09 15:00:18 +0100

    Initial Commit

---
+++ Zante/ZanteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZanteApp.swift
+//  Zante
+//
+//  Created by Anshjyot Singh on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZanteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
