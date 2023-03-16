4bf6542edd4b451b75c5d44f8ada1618ec64b489
Kolaveli1 <vandadazar@hotmail.com>
2023-03-16 15:26:45 +0100

    din far

--- Zante/View/LogInView.swift
+++ Zante/View/LogInView.swift
@@ -15,16 +15,21 @@ struct LogInView: View {
       NavigationView {
 
         VStack(spacing: 20){
-          Image(systemName: "music.note").font(.system(size: 60, weight: .black, design: .default))
+          Image(systemName: "music.note").font(.system(size: 70, weight: .black, design: .default))
 
           VStack(alignment: .leading){
             Text("Welcome Back").font(.system(size: 34, weight: .heavy))
-            Text("Log In to Continue").font(.system(size: 17, weight: .medium))
+                  .offset(x: -35, y: 30)
+            Text("Log In to Continue:").font(.system(size: 17, weight: .medium))
+                  .offset(x: -30, y: 40)
           }
 
             FormField(value: $email, icon: "envelope.fill", placeholder: "E-mail")
+                .offset(y: 40)
 
             FormField(value: $password, icon: "lock.fill", placeholder: "Password", isSecure: true)
+                .offset(y: 10)
+                
 
             Button(action: {}){
               Text("Log In").font(.title)
@@ -32,7 +37,7 @@ struct LogInView: View {
             }
 
           HStack{
-            Text("New?")
+            Text("New user?")
             NavigationLink(destination: SignUpView()){
               Text("Create an Account.").font(.system(size: 18, weight: .semibold))
             }
